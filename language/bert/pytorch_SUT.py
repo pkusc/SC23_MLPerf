38d2a61e2aeae7676e900544d5ea25898a89cc48
interestingLSY <interestingLSY@gmail.com>
2023-10-29 03:28:23 +0000

    Add support for multi-GPU

--- language/bert/pytorch_SUT.py
+++ language/bert/pytorch_SUT.py
@@ -20,6 +20,7 @@ import json
 import os
 import sys
 import time
+import ray
 sys.path.insert(0, os.path.join(os.getcwd(), "DeepLearningExamples", "PyTorch", "LanguageModeling", "BERT"))
 sys.path.insert(0, os.getcwd())
 
@@ -30,21 +31,65 @@ import transformers
 from transformers import BertConfig, BertForQuestionAnswering, MobileBertForQuestionAnswering
 from squad_QSL import get_squad_QSL
 
+num_gpus = 4
 # model_name = 'origin_pytorch_model'
-model_name = 'mrm8488/mobilebert-uncased-finetuned-squadv1'
+# model_name = 'mrm8488/mobilebert-uncased-finetuned-squadv1'
 # model_name = 'yujiepan/internal.mobilebert-uncased-12blks-squadv1-int8-quantize-embedding'
 # model_name = 'yujiepan/test.mobilebert-uncased-12blks-squadv1-int8-f1-88.77'
 # model_name = 'yujiepan/mobilebert-uncased-squadv1-14blocks-structured39.8-int8'
 # model_name = 'yujiepan/test.mobilebert-uncased-12blks-squadv1-int8-f1-90.31'
 # model_name = 'neuralmagic/mobilebert-uncased-finetuned-squadv1'
+model_name = 'csarron/mobilebert-uncased-squad-v1'
+# model_name = 'google/mobilebert-uncased'
+
+@ray.remote(num_gpus=1)
+class BERT_PyTorch_Worker():
+    def __init__(self, worker_id, bert_config):
+        torch.cuda.set_device(0)
+        print(f"Initializing worker {worker_id} on GPU {torch.cuda.get_device_name()}")
+        self.dev = torch.device("cuda:0")
+
+        print("Loading PyTorch model...")
+        print(f'Using {model_name}')
+        if model_name == 'origin_pytorch_model':
+            self.model = BertForQuestionAnswering(bert_config)
+            self.model.to(self.dev)
+            self.model.eval()
+            model_file = os.environ.get("ML_MODEL_FILE_WITH_PATH", "build/data/bert_tf_v1_1_large_fp32_384_v2/model.pytorch")
+            self.model.load_state_dict(torch.load(model_file), strict=False)
+        else:
+            # We set proxy here to let transformers download the model from HuggingFace
+            # Thanks Yuanhang Sun for the proxy!
+            # Fuck GFW.
+            os.environ["http_proxy"] = "http://115.27.161.58:7890"
+            os.environ["https_proxy"] = "http://115.27.161.58:7890"
+            os.environ["all_proxy"] = "http://115.27.161.58:7890"
+            self.model = MobileBertForQuestionAnswering.from_pretrained(model_name)
+            self.model.to(self.dev)
+    
+    def init_ready(self) -> int:
+        return 1
+    
+    def issue_queries(self, input_ids: torch.tensor, attention_mask: torch.tensor, token_type_ids: torch.tensor) -> np.array:
+        with torch.no_grad():
+            input_ids = input_ids.to(self.dev)
+            attention_mask = attention_mask.to(self.dev)
+            token_type_ids = token_type_ids.to(self.dev)
+            model_output = self.model.forward(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+            start_scores = model_output.start_logits
+            end_scores = model_output.end_logits
+            output = torch.stack([start_scores, end_scores], axis=-1).cpu().numpy()
+            return output
+
 
 class BERT_PyTorch_SUT():
     def __init__(self, args):
+        # Read BERT config
         print("Loading BERT configs...")
         with open("bert_config.json") as f:
             config_json = json.load(f)
 
-        config = BertConfig(
+        bert_config = BertConfig(
             attention_probs_dropout_prob=config_json["attention_probs_dropout_prob"],
             hidden_act=config_json["hidden_act"],
             hidden_dropout_prob=config_json["hidden_dropout_prob"],
@@ -56,91 +101,66 @@ class BERT_PyTorch_SUT():
             num_hidden_layers=config_json["num_hidden_layers"],
             type_vocab_size=config_json["type_vocab_size"],
             vocab_size=config_json["vocab_size"])
+        
+        # Initialize workers
+        self.workers = []
+        for i in range(num_gpus):
+            self.workers.append(BERT_PyTorch_Worker.remote(i, bert_config))
+        for worker in self.workers:
+            ray.get(worker.init_ready.remote())
 
-        self.dev = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-        self.version = transformers.__version__
-
-        print("Loading PyTorch model...")
-        print(f'Using {model_name}')
-        if model_name == 'origin_pytorch_model':
-            self.model = BertForQuestionAnswering(config)
-            self.model.to(self.dev)
-            self.model.eval()
-            model_file = os.environ.get("ML_MODEL_FILE_WITH_PATH", "build/data/bert_tf_v1_1_large_fp32_384_v2/model.pytorch")
-            self.model.load_state_dict(torch.load(model_file), strict=False)
-        else:
-            self.model = MobileBertForQuestionAnswering.from_pretrained(model_name)
-            self.model.to(self.dev)
-
-        print("Constructing SUT...")
+        # Initialize SUT (System Under Test)
         self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries)
-        print("Finished constructing SUT.")
 
+        # Initialize QSL (Query Sample Library)
         self.qsl = get_squad_QSL(args.max_examples)
 
     def issue_queries(self, query_samples):
-        with torch.no_grad():
-            num_query_samples = len(query_samples)
+        num_query_samples = len(query_samples)
 
-            load_data_start_time = time.time()
-            input_ids = []
-            attention_mask = []
-            token_type_ids = []
+        load_data_start_time = time.time()
+        input_ids = []
+        attention_mask = []
+        token_type_ids = []
 
-            for i in range(num_query_samples):
-                eval_features = self.qsl.get_features(query_samples[i].index)
-                input_ids.append(eval_features.input_ids)
-                attention_mask.append(eval_features.input_mask)
-                token_type_ids.append(eval_features.segment_ids)
-            
-            input_ids = torch.tensor(input_ids, device=self.dev, dtype=torch.int64)
-            attention_mask = torch.tensor(attention_mask, device=self.dev, dtype=torch.int64)
-            token_type_ids = torch.tensor(token_type_ids, device=self.dev, dtype=torch.int64)
-            load_data_end_time = time.time()
+        for i in range(num_query_samples):
+            eval_features = self.qsl.get_features(query_samples[i].index)
+            input_ids.append(eval_features.input_ids)
+            attention_mask.append(eval_features.input_mask)
+            token_type_ids.append(eval_features.segment_ids)
+        
+        input_ids = torch.tensor(input_ids, dtype=torch.int64)
+        attention_mask = torch.tensor(attention_mask, dtype=torch.int64)
+        token_type_ids = torch.tensor(token_type_ids, dtype=torch.int64)
 
-            forward_time_start = time.time()
-            model_output = self.model.forward(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-            torch.cuda.synchronize()
-            forward_time_end = time.time()
+        input_ids = torch.tensor_split(input_ids, num_gpus)
+        attention_mask = torch.tensor_split(attention_mask, num_gpus)
+        token_type_ids = torch.tensor_split(token_type_ids, num_gpus)
 
-            submit_time_start = time.time()
-            if self.version >= '4.0.0':
-                start_scores = model_output.start_logits
-                end_scores = model_output.end_logits
-            else:
-                start_scores, end_scores = model_output
-            output = torch.stack([start_scores, end_scores], axis=-1).cpu().numpy()
+        load_data_end_time = time.time()
 
-            for i in range(num_query_samples):
-                response_array = array.array("B", output[i].tobytes())
-                bi = response_array.buffer_info()
-                response = lg.QuerySampleResponse(query_samples[i].id, bi[0], bi[1])
-                lg.QuerySamplesComplete([response])
-            submit_time_end = time.time()
+        forward_time_start = time.time()
+        model_output_handlers = []
+        for worker_id in range(len(self.workers)):
+            handler = self.workers[worker_id].issue_queries.remote(input_ids[worker_id], attention_mask[worker_id], token_type_ids[worker_id])
+            model_output_handlers.append(handler)
+        model_output = ray.get(model_output_handlers)
+        forward_time_end = time.time()
 
-            print(f"Time to load data: {str(load_data_end_time - load_data_start_time)} s")
-            print(f"Time to forward: {str(forward_time_end - forward_time_start)} s")
-            print(f"Time to submit: {str(submit_time_end - submit_time_start)} s")
+        output = np.concatenate(model_output, axis=0)
 
-        # with torch.no_grad():
-        #     for i in range(len(query_samples)):
-        #         print("Processing query " + str(i) + " of " + str(len(query_samples)))
-        #         eval_features = self.qsl.get_features(query_samples[i].index)
-        #         print(eval_features.input_ids)
-        #         model_output = self.model.forward(input_ids=torch.tensor(eval_features.input_ids, device=self.dev, dtype=torch.int64).unsqueeze(0),
-        #             attention_mask=torch.tensor(eval_features.input_mask, device=self.dev, dtype=torch.int64).unsqueeze(0),
-        #             token_type_ids=torch.tensor(eval_features.segment_ids, device=self.dev, dtype=torch.int64).unsqueeze(0))
-        #         if self.version >= '4.0.0':
-        #             start_scores = model_output.start_logits
-        #             end_scores = model_output.end_logits
-        #         else:
-        #             start_scores, end_scores = model_output
-        #         output = torch.stack([start_scores, end_scores], axis=-1).squeeze(0).cpu().numpy()
+        submit_time_start = time.time()
 
-        #         response_array = array.array("B", output.tobytes())
-        #         bi = response_array.buffer_info()
-        #         response = lg.QuerySampleResponse(query_samples[i].id, bi[0], bi[1])
-        #         lg.QuerySamplesComplete([response])
+        for i in range(num_query_samples):
+            response_array = array.array("B", output[i].tobytes())
+            bi = response_array.buffer_info()
+            response = lg.QuerySampleResponse(query_samples[i].id, bi[0], bi[1])
+            lg.QuerySamplesComplete([response])
+        submit_time_end = time.time()
+
+        print(f"Time to load data: {str(load_data_end_time - load_data_start_time)} s")
+        print(f"Time to forward: {str(forward_time_end - forward_time_start)} s")
+        print(f"Time to submit: {str(submit_time_end - submit_time_start)} s")
 
     def flush_queries(self):
         pass

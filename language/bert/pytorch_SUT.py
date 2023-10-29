a95c0a51bc3a10f05dcaf1f5775fad7789a3191c
interestingLSY <interestingLSY@gmail.com>
2023-10-29 05:46:38 +0000

    Optimiza communication (failed)

--- language/bert/pytorch_SUT.py
+++ language/bert/pytorch_SUT.py
@@ -21,10 +21,27 @@ import os
 import sys
 import time
 import ray
+from typing import List
 sys.path.insert(0, os.path.join(os.getcwd(), "DeepLearningExamples", "PyTorch", "LanguageModeling", "BERT"))
 sys.path.insert(0, os.getcwd())
 
+loadgen_path = "/home/intlsy/CM/repos/local/cache/fa9180d70a5e4e1f/"
+inference_bert_path = "/home/intlsy/CM/repos/local/cache/8e90af0a933b4703/inference/language/bert"
+
+# We set env vars here to let ray workers find the loadgen library
+ray.init(runtime_env = {
+    "env_vars": {
+        "LD_LIBRARY_PATH": os.path.join(loadgen_path, 'install', 'lib'),
+        "DYLD_FALLBACK_LIBRARY_PATH": os.path.join(loadgen_path, 'install', 'lib'),
+        "PYTHONPATH": os.path.join(loadgen_path, 'install', 'python'),
+        "C_INCLUDE_PATH": os.path.join(loadgen_path, 'install', 'include'),
+        "CPLUS_INCLUDE_PATH": os.path.join(loadgen_path, 'install', 'include'),
+    }
+})
+
+# ray.init(runtime_env={"conda": "mlperf"})
 import mlperf_loadgen as lg
+
 import numpy as np
 import torch
 import transformers
@@ -42,12 +59,13 @@ num_gpus = 4
 model_name = 'csarron/mobilebert-uncased-squad-v1'
 # model_name = 'google/mobilebert-uncased'
 
-@ray.remote(num_gpus=1)
+@ray.remote(num_cpus=1, num_gpus=1)
 class BERT_PyTorch_Worker():
-    def __init__(self, worker_id, bert_config):
+    def __init__(self, worker_id, bert_config, max_examples: int):
         torch.cuda.set_device(0)
         print(f"Initializing worker {worker_id} on GPU {torch.cuda.get_device_name()}")
         self.dev = torch.device("cuda:0")
+        os.chdir(inference_bert_path)   # We chdir() here or the QSL cannot find the vocabuary path
 
         print("Loading PyTorch model...")
         print(f'Using {model_name}')
@@ -61,24 +79,40 @@ class BERT_PyTorch_Worker():
             # We set proxy here to let transformers download the model from HuggingFace
             # Thanks Yuanhang Sun for the proxy!
             # Fuck GFW.
-            os.environ["http_proxy"] = "http://115.27.161.58:7890"
-            os.environ["https_proxy"] = "http://115.27.161.58:7890"
-            os.environ["all_proxy"] = "http://115.27.161.58:7890"
+            # proxy_addr = "http://115.27.161.58:7890"
+            proxy_addr = "http://10.129.196.146:20171"
+            os.environ["http_proxy"] = proxy_addr
+            os.environ["https_proxy"] = proxy_addr
+            os.environ["all_proxy"] = proxy_addr
             self.model = MobileBertForQuestionAnswering.from_pretrained(model_name)
             self.model.to(self.dev)
+        
+        # Initialize QSL (Query Sample Library)
+        self.qsl = get_squad_QSL(max_examples)
     
     def init_ready(self) -> int:
         return 1
     
-    def issue_queries(self, input_ids: torch.tensor, attention_mask: torch.tensor, token_type_ids: torch.tensor) -> torch.tensor:
+    def issue_queries(self, indexes: List[int]) -> torch.tensor:
         with torch.no_grad():
-            input_ids = input_ids.to(torch.int32).to(self.dev)
-            attention_mask = attention_mask.to(torch.int32).to(self.dev)
-            token_type_ids = token_type_ids.to(torch.int32).to(self.dev)
+            input_ids = []
+            attention_mask = []
+            token_type_ids = []
+
+            for index in indexes:
+                eval_features = self.qsl.get_features(index)
+                input_ids.append(eval_features.input_ids)
+                attention_mask.append(eval_features.input_mask)
+                token_type_ids.append(eval_features.segment_ids)
+            
+            input_ids = torch.tensor(input_ids, dtype=torch.int32, device=self.dev)
+            attention_mask = torch.tensor(attention_mask, dtype=torch.int32, device=self.dev)
+            token_type_ids = torch.tensor(token_type_ids, dtype=torch.int32, device=self.dev)
+
             model_output = self.model.forward(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
             start_scores = model_output.start_logits
             end_scores = model_output.end_logits
-            output = torch.stack([start_scores, end_scores], axis=-1).to(torch.float16).cpu()
+            output = torch.stack([start_scores, end_scores], axis=-1).to(torch.float16).cpu()   # We send float16 to save network bandwidth
             return output
 
 
@@ -105,46 +139,28 @@ class BERT_PyTorch_SUT():
         # Initialize workers
         self.workers = []
         for i in range(num_gpus):
-            self.workers.append(BERT_PyTorch_Worker.remote(i, bert_config))
+            self.workers.append(BERT_PyTorch_Worker.remote(i, bert_config, args.max_examples))
         for worker in self.workers:
             ray.get(worker.init_ready.remote())
 
         # Initialize SUT (System Under Test)
         self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries)
 
-        # Initialize QSL (Query Sample Library)
         self.qsl = get_squad_QSL(args.max_examples)
 
     def issue_queries(self, query_samples):
         num_query_samples = len(query_samples)
 
         load_data_start_time = time.time()
-        input_ids = []
-        attention_mask = []
-        token_type_ids = []
-
+        indexes = []
         for i in range(num_query_samples):
-            eval_features = self.qsl.get_features(query_samples[i].index)
-            input_ids.append(eval_features.input_ids)
-            attention_mask.append(eval_features.input_mask)
-            token_type_ids.append(eval_features.segment_ids)
-        
-        # Here we convert those tensors to int16 and int8 to save network BW
-        # After receiving the tensors, we convert them back to int32
-        input_ids = torch.tensor(input_ids, dtype=torch.int16)
-        attention_mask = torch.tensor(attention_mask, dtype=torch.int8)
-        token_type_ids = torch.tensor(token_type_ids, dtype=torch.int8)
-
-        input_ids = torch.tensor_split(input_ids, num_gpus)
-        attention_mask = torch.tensor_split(attention_mask, num_gpus)
-        token_type_ids = torch.tensor_split(token_type_ids, num_gpus)
-
+            indexes.append(query_samples[i].index)
         load_data_end_time = time.time()
 
         forward_time_start = time.time()
         model_output_handlers = []
         for worker_id in range(len(self.workers)):
-            handler = self.workers[worker_id].issue_queries.remote(input_ids[worker_id], attention_mask[worker_id], token_type_ids[worker_id])
+            handler = self.workers[worker_id].issue_queries.remote(indexes[worker_id::num_gpus])
             model_output_handlers.append(handler)
         model_outputs = ray.get(model_output_handlers)
         forward_time_end = time.time()
